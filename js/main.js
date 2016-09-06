a7a2125395e2a3d57a90a29519ab748313a7ee9e
Poputa Favian-Ioel <favianioel@gmail.com>
2016-09-06 21:50:54 +0300

    libraries , delete bootstrap , use full page js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
     $('#fullpage').fullpage();
+
+// scroll page svg show on mousemove
+    var i = null;
+	$("html").mousemove(function() {
+    	clearTimeout(i);
+    	$(".scroll-down").show();
+    	i = setTimeout(function () {
+        	$(".scroll-down").fadeOut();
+    	}, 1000);
+	}).mouseleave(function() {
+    	clearTimeout(i);
+    	$(".scroll-down").fadeOut();  
+	});
 });
